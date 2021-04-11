2fdbc409ddc430bf3f7e0c3535eb2536fab554e5
Jericho Limpin <38738650+Bubble-Rain@users.noreply.github.com>
2021-04-12 09:19:39 +1000

    Update TF record generator

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -80,16 +80,35 @@ def xml_to_csv(path):
     for xml_file in glob.glob(path + '/*.xml'):
         tree = ET.parse(xml_file)
         root = tree.getroot()
+        
         for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
+        	
+            print(root.find('filename').text)
+            print(member[1].tag)
+            #print(member[4])
+            
+            if member[1].tag != 'bndbox':
+            	
+            	value = (root.find('filename').text,
+                	     int(root.find('size')[0].text),
+                    	 int(root.find('size')[1].text),
+                     	member[0].text,
+                     	int(member[4][0].text),
+                     	int(member[4][1].text),
+                     	int(member[4][2].text),
+                     	int(member[4][3].text)
+                     	)
+            
+            else:
+            	value = (root.find('filename').text,
+                	     int(root.find('size')[0].text),
+                    	 int(root.find('size')[1].text),
+                     	member[0].text,
+                     	int(member[1][0].text),
+                     	int(member[1][1].text),
+                     	int(member[1][2].text),
+                     	int(member[1][3].text)
+                     	)
             xml_list.append(value)
     column_name = ['filename', 'width', 'height',
                    'class', 'xmin', 'ymin', 'xmax', 'ymax']
